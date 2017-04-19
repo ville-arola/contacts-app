64b9104e74264bb7cab8eb7d64beb205d765d6f7
ville-arola <1700383@student.saimia.fi>
2017-04-19 15:01:34 +0300

    Sidenav and toolbar added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MdSidenav} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
-  constructor() {}
+  toolbarVisible: boolean;
+  sidenavMode: string;
+
+  @ViewChild('sidenav') sidenav: MdSidenav;
+
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        console.log(event);
+        this.toolbarVisible = event.urlAfterRedirects != '/login';
+      }
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event) {
+    let width = event ? event.target.innerWidth : window.innerWidth;
+    this.sidenavMode = width >= 600 ? 'side' : 'over';
+  }
 
   ngOnInit(): void {
+    this.onWindowResize(null);
+  }
+
+  toggleSidenav(){
+    this.sidenav.toggle();
   }
 }
--- client/src/app/contact/map-dialog/map-dialog.component.ts
+++ client/src/app/contact/map-dialog/map-dialog.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {MdDialogRef} from "@angular/material";
 import {DomSanitizer} from "@angular/platform-browser";
 
@@ -14,7 +14,7 @@ export class MapDialogComponent implements OnInit {
   width: number;
   height: number;
 
-  constructor(public dialog: MdDialogRef<MapDialogComponent>, private sanitizer: DomSanitizer, private elementRef: ElementRef ) { }
+  constructor(public dialog: MdDialogRef<MapDialogComponent>, private sanitizer: DomSanitizer) { }
 
   closeMap() {
     this.dialog.close();
@@ -26,11 +26,11 @@ export class MapDialogComponent implements OnInit {
 
   ngOnInit() {
     this.mapUrl = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDmUcAU7BWA5VkyY5KvP84kM8fc_bdXMoM&q=' + this.address;
-    this.onWindowResize(null);
+    this.onWindowResize();
   }
 
   @HostListener('window:resize', ['$event'])
-  onWindowResize(event) {
+  onWindowResize() {
     let w = document.body.clientWidth;
     let h = window.innerHeight;
     this.width = Math.min(w*0.6, 600);
